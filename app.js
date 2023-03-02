e37ebffeaab3338ab1efb01ff2d0b6b6187546fa
Vibhanshu Jain <cs19b1027@iiitr.ac.in>
2023-03-02 16:36:50 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+}
+);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+}
+);
+
